578a541272594955ee48bc373db7ea1f9dbb228f
bodykhairy226 <bodykhairy226@gmail.com>
2026-04-04 17:00:01 +0200

    fix mongo env variable

--- routes/.trans.js
+++ routes/.trans.js
@@ -6,13 +6,13 @@ const Otp = require("../models/Otp"); // مهم المسار
 
 router.post("/generate-otp", async (req, res) => {
   try {
-    const { atmCode, pin, amount } = req.body;
 
-    if (!atmCode || !pin || !amount) {
+        const { atmCode, pin, amount, userId } = req.body;
+
+if (!atmCode || !pin || !amount || !userId) {
       return res.status(400).json({ message: "Missing data" });
     }
 
-    const userId = "TEST_USER_ID";
 
     const otp = Math.floor(100000 + Math.random() * 900000);
 
