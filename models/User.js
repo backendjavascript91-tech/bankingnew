7334779dc62db56e557c2cc81300f32487cc5b81
bodykhairy226 <bodykhairy226@gmail.com>
2026-04-04 18:36:42 +0200

    fix mongoose overwrite model

--- models/User.js
+++ models/User.js
@@ -7,4 +7,4 @@ const userSchema = new mongoose.Schema({
   balance: Number
 });
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.models.User || mongoose.model("User", userSchema);
