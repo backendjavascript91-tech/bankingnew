421db7628afb20f36a2752c6844bfc7b8c59124f
bodykhairy226 <bodykhairy226@gmail.com>
2026-04-23 11:57:44 +0200

    fix mongo env variable

--- public/js/login.js
+++ public/js/login.js
@@ -68,7 +68,7 @@ const userData = {
 
       localStorage.setItem("token", data.token);
       localStorage.setItem("currentUser", JSON.stringify(userData));
-sessionStorage.setItem("isLoggedIn", "true");
+localStorage.setItem("isLoggedIn", "true");
      showLoginMessage("Login successful", "success");
       window.location.href = "dashboard.html";
 
